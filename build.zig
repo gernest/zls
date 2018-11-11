e6737e6799cf437e8f9e8b20784b2d79a26d3d0d
gernest <geofreyernest@live.com>
2018-11-11 09:47:09 +0300

    make the project executable

--- build.zig
+++ build.zig
@@ -2,15 +2,14 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("zls", "src/main.zig");
-    lib.setBuildMode(mode);
+    const exe = b.addExecutable("zls", "src/main.zig");
+    exe.setBuildMode(mode);
 
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addCommand(".", b.env_map, [][]const u8.{exe.getOutputPath()});
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(&exe.step);
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-
-    b.default_step.dependOn(&lib.step);
-    b.installArtifact(lib);
+    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
 }
